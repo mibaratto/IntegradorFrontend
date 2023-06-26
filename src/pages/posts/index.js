3e9e3d41c4dd015ddb86ccf684bd16cdc58a8f91
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-06-26 23:54:50 +0200

    show all posts

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,6 +1,50 @@
-export const PostsPage = () => {
-    return (
-        <h1>Posts Page</h1>
-    )
+import { Header } from "../../components/Header/Header"
+import { ContainerPostsPage, CardPost} from "./styled"
+import { useEffect, useState } from "react"
+import { PostsFeed } from "../../constants"
+import { goToPostWithCommentsPage } from "../../routes/coordinator"
+import { useNavigate } from "react-router-dom"
 
+export const PostsPage = () => {
+    const navigate = useNavigate()
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        PostsFeed()
+        .then(data => {
+            setPosts(data)
+            console.log(data)
+        })
+        .catch((error)=> {
+            console.log(error)
+        })
+    }, [])
+
+    const onClickCard = (id) => {
+        goToPostWithCommentsPage(navigate, id)
+    }
+
+    return (
+        <>
+            <Header/>
+            <di>
+                <textarea
+                    placeholder="Escreva seu post"
+                />
+                <div>
+                    <button>Postar</button>
+                </div>
+                
+            </di>
+            <ContainerPostsPage>
+                    {posts.map((post, index) => (
+                        <CardPost key={index} onClick={() => onClickCard(post.id)}>
+                            <h3>{post.content}</h3>
+                            <h3>{post.id}</h3>
+                            <h3>{post.creator.name}</h3>
+                        </CardPost>
+                    ))}
+            </ContainerPostsPage>
+        </>
+    )
 }
