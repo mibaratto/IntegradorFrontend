a410eb66a98f47657abd00423f4de9b196ae6195
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-06-27 17:59:16 +0200

    adding post with comments

--- src/pages/post-with-comments/index.js
+++ src/pages/post-with-comments/index.js
@@ -1,6 +1,32 @@
-export const PostWithCommentsPage = () => {
-    return (
-        <h1>Post With Comments Page</h1>
-    )
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { PostWithComments } from "../../constants"
 
+export const PostWithCommentsPage = () => {
+    const {id} = useParams()
+    const [postAndComments, setPostAndComments] = useState()
+
+    useEffect(() => {
+        PostWithComments(id)
+        .then(data => {
+            setPostAndComments(data)
+        })
+        .catch(error => alert(error.response.data.message))
+    },[])
+
+    let onlyPost = JSON.stringify(postAndComments.postWithComments.post.content)
+    let onlyComments = JSON.stringify(postAndComments.postWithComments.comments
+        .map(comment => comment.content))
+    return (
+        <>
+            <h1>{onlyPost}</h1>
+            <br></br>
+            <p>{JSON.stringify(postAndComments.postWithComments.post.content)}</p>
+            <br></br>
+            <p>{onlyComments}</p>
+            <br></br>
+            {JSON.stringify(postAndComments.postWithComments.comments
+                .map(comment => comment.content + comment.id))}
+        </>
+    )
 }
